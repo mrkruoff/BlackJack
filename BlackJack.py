a5ee1cc53a0c70a892fe732745374e15c9e569f0
mrkruoff <ruoffm@oregonstate.edu>
2017-10-10 15:07:25 -0700

    Updates to BlackJack file
    Basic action functions implemented

--- BlackJack.py
+++ BlackJack.py
@@ -1,4 +1,63 @@
 from DeckofCards import *
 
+def cardValue(card):
+    if type(card[0])==int:
+        return card[0]
+    elif card[0]=="A":
+        return 11
+    else:
+        return 10
+
+def handvalue(hand):
+    """iterates through hand returns values"""
+    value=0
+    for i in range(len(hand)):
+        value+=cardValue(hand[i])
+    return value
+    
+
+def playerHandDelt(hand):
+    """takes in hand as a list, iterates through
+    and returns a value of the value of hand"""
+    value=handvalue(hand)
+    printHand(hand)
+    return value
+    
+def dealerHandDelt(hand):
+    """Reviews Dealers initial hand, if not 21, prints first card, if 21 ends round
+    """
+    value=handvalue(hand)
+    if value ==21:
+        printHand(hand)
+        return value
+    else:
+        card=hand[0]
+        printCard(card)
+        print("XX")
+        
+def printCard(card):
+    print(str(card[0])+card[1]+" ", end='')
+
+
+def printHand(hand):
+    """Prints out hand"""
+    for i in range(len(hand)):
+        current=hand[i]
+        printCard(current)
+    print("")
+
+def deal(deck):
+    """Draws a card and returns card"""
+    card=drawCard(deck)
+    return card
+
+def round(deck):
+    """plays a round of plackJack, prints out winner"""
+
+
 if __name__=="__main__":
+
+        
+        
+    
 
